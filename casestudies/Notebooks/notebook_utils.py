e945c00b1c9e4fdfbadcfb52adb472a2a33f2e66
VargFo <varg.forland@gmail.com>
2025-03-11 12:17:34 +0100

    Two new notebooks: Frequency and voltage stability. Also new structure for figure folders and some small fixes i general.

--- casestudies/Notebooks/notebook_utils.py
+++ casestudies/Notebooks/notebook_utils.py
@@ -4,8 +4,8 @@ from matplotlib.patches import Arc
 import numpy as np
 from scipy.integrate import quad
 from scipy.optimize import fsolve
-from ipywidgets import interact, FloatSlider
-
+from ipywidgets import interact, FloatSlider, widgets
+from IPython.display import display
 
 """
 ------------------------------------------------------------------------------------------------------------------------------------------
@@ -386,7 +386,11 @@ def update_plot(delta_clear, P_m, P_e_sc, E, Vs, x_dt):
     plt.show()
 
 
-
+'''
+------------------------------------------------------------------------------------------------------------------------------------------
+-------------------------------------------------------  VOLTAGE STABILITY UTILS  --------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------------------------
+'''
 
 def calculate_open_loop_transfer_function(H, D, w_N, K_E_t):
     return ctrl.TransferFunction([1, 0], [2*H, D, w_N*K_E_t], name='Open Loop')
@@ -416,3 +420,408 @@ def calc_damping_and_freq(eigenvalue):
     frequency = np.abs(eigenvalue.imag) / (2 * np.pi)
     print(f'Damping of eigenvalue {eigenvalue} is: {damping:.2f} % and frequency: {frequency:.2f} Hz')
     return damping, frequency
+
+# NOTEBOOK 7
+
+# Define the function to update the transfer functions and plots
+def update_transfer_functions_AVR(k_p, k_i, k_d, T_E, K_e, K_3, T_d0_t, K_6, T_m):
+    # AVR block:
+
+    AVR_PID = ctrl.tf( ctrl.tf([k_p], [1])+ctrl.tf([k_i], [1, 0])+ctrl.tf([k_d, 0], [1]) , name = r"G_{avr}(s)")
+
+    #Exciter block:
+    # Exciter time constant, assume not tunable, T_e = 0.1 TOPS standard value
+    Gexc = ctrl.tf([K_e], [T_E, 1], name = r"G_{exc}(s)")
+
+    # Generator block:
+    Ggen = ctrl.tf([K_3], [K_3*T_d0_t, 1], name = r"G_{gen}(s)")
+
+    # Feedback Block 
+    Hmeas = ctrl.tf([1], [T_m, 1], name = r"H_{meas}(s)")
+
+    G = ctrl.tf(Gexc*Ggen*AVR_PID*K_6, name = r"G(s)")
+    L = ctrl.tf(G*Hmeas, name = r"L(s)")
+    CL = ctrl.tf(G/(1+L), name = r"CL(s)")
+
+    return L,CL
+
+def poles_interactive_plot_AVR(CL):
+
+    poles = ctrl.poles(CL)
+    plt.figure()
+    plt.scatter(poles.real, poles.imag, marker='x', color='red')
+    plt.axhline(0, color='black', lw=0.5)
+    plt.axvline(0, color='black', lw=0.5)
+    plt.xlabel('Real Part')
+    plt.ylabel('Imaginary Part')
+    plt.title('Poles of the Closed-Loop Transfer Function')
+    plt.grid(True)
+
+    # Annotate damping and frequency
+    for eig in poles:
+        if abs(eig) < 1e-10:
+            continue
+        damping = -eig.real/abs(eig)
+        if damping < 0.99:
+            frequency = np.abs(eig.imag) / (2 * np.pi)
+            plt.annotate(f'{damping*100:.2f} %, {frequency:.2f} Hz', (eig.real, eig.imag), textcoords="offset points", xytext=(10, -10), ha='center')
+
+    # Mark the area 0.1-2 Hz and below 10% damping indicating electromechanical modes
+    freq_range = np.linspace(0.1, 2, 1000)
+    damping_ratio = 0.1
+    real_part = -damping_ratio * 2 * np.pi * freq_range
+    imag_part = 2 * np.pi * freq_range * np.sqrt(1 - damping_ratio**2)
+    plt.fill_betweenx(imag_part, real_part, 0, color='yellow', alpha=0.3, label='Electromechanical Modes (0.1-2 Hz, <10% damping)')
+    plt.fill_betweenx(-imag_part, real_part, 0, color='yellow', alpha=0.3)
+    plt.legend(loc='upper left')
+
+    # plt.show()
+
+def bode_interactive_plot_AVR(CL):
+
+    response = ctrl.frequency_response(CL)
+    ctrl.bode_plot(response, initial_phase=0, display_margins=True, dB=True)
+    # plt.show()
+
+def stepresponse_interactive_plot_AVR(CL):
+
+
+    t, y = ctrl.step_response(CL)
+    plt.figure()
+    plt.plot(t, y)
+    plt.xlabel('Time [s]')
+    plt.ylabel('Output')
+    plt.title('Step Response of the Closed-Loop Transfer Function')
+    # plt.show()
+
+def interactive_AVR(plot_type, kp, ki, kd, K_e, T_E, K_3, T_d0_t, K_6, T_m):
+    L, CL = update_transfer_functions_AVR(kp, ki, kd, T_E, K_e, K_3, T_d0_t, K_6, T_m)
+    if plot_type == "POLES":
+        poles_interactive_plot_AVR(CL)
+    elif plot_type == "BODE":
+        bode_interactive_plot_AVR(CL)
+    elif plot_type == "STEP":
+        stepresponse_interactive_plot_AVR(CL)
+    # plt.show()
+
+
+def display_interactive_AVR():
+    ui = widgets.HBox([
+        widgets.VBox([
+            widgets.Dropdown(options=["POLES", "BODE", "STEP"], description='Plot Type:'),
+            widgets.FloatSlider(value=10.0, min=0.0, max=100.0, step=0.05, description='kp'),
+            widgets.FloatSlider(value=2.0, min=0.0, max=100.0, step=0.05, description='ki'),
+            widgets.FloatSlider(value=3.0, min=0.0, max=100.0, step=0.05, description='kd'),
+        ]),
+        widgets.VBox([
+            widgets.FloatText(value=1.0, description='K_e'),
+            widgets.FloatText(value=0.1, description='T_E'),
+            widgets.FloatText(value=0.3, description='K_3'),
+            widgets.FloatText(value=8.0, description='T_d0_t'),
+            widgets.FloatText(value=0.4, description='K_6'),
+            widgets.FloatText(value=0.2, description='T_m'),
+        ])
+    ])
+    out = widgets.interactive_output(interactive_AVR, {
+        'plot_type': ui.children[0].children[0],
+        'kp': ui.children[0].children[1],
+        'ki': ui.children[0].children[2],
+        'kd': ui.children[0].children[3],
+        'K_e': ui.children[1].children[0],
+        'T_E': ui.children[1].children[1],
+        'K_3': ui.children[1].children[2],
+        'T_d0_t': ui.children[1].children[3],
+        'K_6': ui.children[1].children[4],
+        'T_m': ui.children[1].children[5],
+    })
+
+    #Error here; Display function is a Notebook function i.e. this only works when called in a jupyter notebook
+    display(ui, out)
+
+
+def load_model_NO_AVR():
+    return {
+        'base_mva': 100, #'base_mva': system base/nominal complex power in [MVA].
+        'f': 50, #'f': system frequency in [Hz].
+        'slack_bus': 'B3', #'slack_bus': reference busbar with zero phase angle.
+
+        'buses': [
+            ['name',    'V_n'],
+            ['B1',         10],
+            ['B2',        132],
+            ['B3',        132],
+        ],
+        #'V_n': base/nominal voltage in [kV].
+
+        'lines': [
+            ['name',  'from_bus', 'to_bus',   'length',   'S_n',  'V_n',  'unit', 'R',    'X',   'B'],
+            ['L2-3_1',        'B2',     'B3',        250,      100,    132,    'PF',   0,    0.2,     0],
+            ['L2-3_2',        'B2',     'B3',        250,      110,    132,    'PF',   0,    0.2,     0],
+        ],
+
+
+        'transformers': [
+            ['name', 'from_bus', 'to_bus', 'S_n', 'V_n_from', 'V_n_to', 'R', 'X'],
+            ['T1',         'B1',     'B2',    100,         10,      132,   0, 0.1],
+        ],
+
+        'generators': {
+            'GEN': [
+                ['name',   'bus',  'S_n',  'V_n',    'P',    'V',      'H',    'D',    'X_d',  'X_q',  'X_d_t',    'X_q_t',    'X_d_st',   'X_q_st',   'T_d0_t',   'T_q0_t',   'T_d0_st',  'T_q0_st'],
+                ['G1',      'B1',     100,     10,     80,  1.1314,      5,      0,     1.0,   0.66,    0.3,      0.66,       0.254,      0.273,       2.49,      10000,         0.06,       0.15],
+                ['IB',      'B3',    1e8,    132,    -80,  1,      1e5,      0,     1.05,   0.66,    0.328,      0.66,       0.254,      0.273,       2.49,      10000,         0.06,       0.15],
+            ],
+        },
+
+    }
+
+def load_model_wAVR(K, T_a, T_b, T_e, E_min, E_max):
+    return {
+        'base_mva': 100, #'base_mva': system base/nominal complex power in [MVA].
+        'f': 50, #'f': system frequency in [Hz].
+        'slack_bus': 'B3', #'slack_bus': reference busbar with zero phase angle.
+
+        'buses': [
+            ['name',    'V_n'],
+            ['B1',         10],
+            ['B2',        132],
+            ['B3',        132],
+        ],
+        #'V_n': base/nominal voltage in [kV].
+
+        'lines': [
+            ['name',  'from_bus', 'to_bus',   'length',   'S_n',  'V_n',  'unit', 'R',    'X',   'B'],
+            ['L2-3_1',        'B2',     'B3',        250,      100,    132,    'PF',   0,    0.2,     0],
+            ['L2-3_2',        'B2',     'B3',        250,      110,    132,    'PF',   0,    0.2,     0],
+        ],
+
+
+        'transformers': [
+            ['name', 'from_bus', 'to_bus', 'S_n', 'V_n_from', 'V_n_to', 'R', 'X'],
+            ['T1',         'B1',     'B2',    100,         10,      132,   0, 0.1],
+        ],
+
+        'generators': {
+            'GEN': [
+                ['name',   'bus',  'S_n',  'V_n',    'P',    'V',      'H',    'D',    'X_d',  'X_q',  'X_d_t',    'X_q_t',    'X_d_st',   'X_q_st',   'T_d0_t',   'T_q0_t',   'T_d0_st',  'T_q0_st'],
+                ['G1',      'B1',     100,     10,     80,  1.1314,      5,      0,     1.0,   0.66,    0.3,      0.66,       0.254,      0.273,       2.49,      10000,         0.06,       0.15],
+                ['IB',      'B3',    1e8,    132,    -80,  1,      1e5,      0,     1.05,   0.66,    0.328,      0.66,       0.254,      0.273,       2.49,      10000,         0.06,       0.15],
+            ],
+        },
+
+        'avr': {
+            'SEXS': [
+                ['name', 'gen', 'K', 'T_a', 'T_b', 'T_e', 'E_min', 'E_max'],
+                ['AVR1', 'G1', K, T_a, T_b, T_e, E_min, E_max],
+            ]
+        },
+
+    }
+
+
+
+
+'''
+------------------------------------------------------------------------------------------------------------------------------------------
+-------------------------------------------------------  FREQUENCY STABILITY UTILS  --------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------------------------
+'''
+
+
+
+def update_transfer_functions_GOV(k_p, k_i, k_d, rho, T_w, K_1, D, H, w_N):
+    #PID block:
+    G_pid = ctrl.tf( ctrl.tf([k_p], [1])+ctrl.tf([k_i], [1, 0])+ctrl.tf([k_d, 0], [1]) , name = r"G_{pid}(s)")
+    # Turbine block:
+    G_t = ctrl.tf([-T_w,1], [0.5*T_w, 1], name = r"G_{t}(s)")
+    # GOV block:
+    G_GOV = ctrl.tf(G_t*G_pid/(1+rho*G_t+G_pid), name = r"G_{GOV}(s)")
+    # GEN block:
+    G_gen = ctrl.tf([1,0], [2*H, D, K_1*w_N], name = r"G_{gen}(s)")
+    # Open loop:
+    L = ctrl.tf(G_GOV*G_gen, name = r"L(s)")
+    # Closed loop:
+    CL = ctrl.tf(L/(1+L), name = r"CL(s)")
+    return L, CL
+
+def poles_interactive_plot_GOV(CL):
+
+    poles = ctrl.poles(CL)
+    plt.figure()
+    plt.scatter(poles.real, poles.imag, marker='x', color='red')
+    plt.axhline(0, color='black', lw=0.5)
+    plt.axvline(0, color='black', lw=0.5)
+    plt.xlabel('Real Part')
+    plt.ylabel('Imaginary Part')
+    plt.title('Poles of the Closed-Loop Transfer Function')
+    plt.grid(True)
+
+    # Annotate damping and frequency
+    min_damping = float('inf')
+    min_damping_pole = None
+    for eig in poles:
+        if abs(eig) < 1e-10:
+            continue
+        damping = -eig.real/abs(eig)
+        if damping < min_damping:
+            min_damping = damping
+            min_damping_pole = eig
+
+    if min_damping_pole is not None and min_damping < 0.99:
+        frequency = np.abs(min_damping_pole.imag) / (2 * np.pi)
+        plt.annotate(f'{min_damping*100:.2f} %, {frequency:.2f} Hz', (min_damping_pole.real, min_damping_pole.imag), textcoords="offset points", xytext=(10, -10), ha='center')
+
+    # Mark the area 0.1-2 Hz and below 10% damping indicating electromechanical modes
+    freq_range = np.linspace(0.1, 2, 1000)
+    damping_ratio = 0.1
+    real_part = -damping_ratio * 2 * np.pi * freq_range
+    imag_part = 2 * np.pi * freq_range * np.sqrt(1 - damping_ratio**2)
+    plt.fill_betweenx(imag_part, real_part, 0, color='yellow', alpha=0.3, label='Electromechanical Modes (0.1-2 Hz, <10% damping)')
+    plt.fill_betweenx(-imag_part, real_part, 0, color='yellow', alpha=0.3)
+    plt.legend(loc='upper left')
+
+    # plt.show()
+
+def bode_interactive_plot_GOV(CL):
+    response = ctrl.frequency_response(CL)
+    ctrl.bode_plot(response, initial_phase=0, display_margins=True, dB=True)
+    # plt.show()
+
+def stepresponse_interactive_plot_GOV(CL):
+    t, y = ctrl.step_response(CL)
+    plt.figure()
+    plt.plot(t, y)
+    plt.xlabel('Time [s]')
+    plt.ylabel('Output')
+    plt.title('Step Response of the Closed-Loop Transfer Function')
+    # plt.show()
+
+def interactive_GOV(plot_type, k_p, k_i, k_d, rho, T_w, K_1, D, H, w_N):
+    L, CL = update_transfer_functions_GOV(k_p, k_i, k_d, rho, T_w, K_1, D, H, w_N)
+    if plot_type == "POLES":
+        poles_interactive_plot_GOV(CL)
+    elif plot_type == "BODE":
+        bode_interactive_plot_GOV(CL)
+    elif plot_type == "STEP":
+        stepresponse_interactive_plot_GOV(CL)
+    plt.show()
+
+def display_interactive_GOV():
+    ui = widgets.HBox([
+        widgets.VBox([
+            widgets.Dropdown(options=["POLES", "BODE", "STEP"], description='Plot Type:'),
+            widgets.FloatSlider(value=2.5, min=0.0, max=100.0, step=0.05, description='kp'),
+            widgets.FloatSlider(value=0.5, min=0.0, max=100.0, step=0.05, description='ki'),
+            widgets.FloatSlider(value=0, min=0.0, max=100.0, step=0.05, description='kd'),
+            widgets.FloatSlider(value=0.06, min=0.0, max=1.0, step=0.01, description='rho')
+        ]),
+        widgets.VBox([
+            widgets.FloatText(value=1, description='T_w'),
+            widgets.FloatText(value=1.75, description='K_1'),
+            widgets.FloatText(value=10, description='D'),
+            widgets.FloatText(value=5, description='H')
+        ])
+    ])
+
+    out = widgets.interactive_output(interactive_GOV, {
+        'plot_type': ui.children[0].children[0],
+        'k_p': ui.children[0].children[1],
+        'k_i': ui.children[0].children[2],
+        'k_d': ui.children[0].children[3],
+        'rho': ui.children[0].children[4],
+        'T_w': ui.children[1].children[0],
+        'K_1': ui.children[1].children[1],
+        'D': ui.children[1].children[2],
+        'H': ui.children[1].children[3],
+        'w_N': widgets.fixed(2 * np.pi * 50)
+    })
+
+    #Error here; Display function is a Notebook function i.e. this only works when called in a jupyter notebook
+    display(ui, out)
+
+
+#  'gov': {
+#      'TGOV1': [
+#          ['name', 'gen', 'R', 'D_t', 'V_min', 'V_max', 'T_1', 'T_2', 'T_3'],
+#          ['GOV1', 'G1', 0.05, 0.02, 0, 1, 0.1, 0.09, 0.2],
+#      ]
+#  },
+
+def load_model_NO_GOV():
+    return {
+        'base_mva': 100, #'base_mva': system base/nominal complex power in [MVA].
+        'f': 50, #'f': system frequency in [Hz].
+        'slack_bus': 'B3', #'slack_bus': reference busbar with zero phase angle.
+
+        'buses': [
+            ['name',    'V_n'],
+            ['B1',         10],
+            ['B2',        132],
+            ['B3',        132],
+        ],
+        #'V_n': base/nominal voltage in [kV].
+
+        'lines': [
+            ['name',  'from_bus', 'to_bus',   'length',   'S_n',  'V_n',  'unit', 'R',    'X',   'B'],
+            ['L2-3_1',        'B2',     'B3',        250,      100,    132,    'PF',   0,    0.2,     0],
+            ['L2-3_2',        'B2',     'B3',        250,      110,    132,    'PF',   0,    0.2,     0],
+        ],
+
+
+        'transformers': [
+            ['name', 'from_bus', 'to_bus', 'S_n', 'V_n_from', 'V_n_to', 'R', 'X'],
+            ['T1',         'B1',     'B2',    100,         10,      132,   0, 0.1],
+        ],
+
+        'generators': {
+            'GEN': [
+                ['name',   'bus',  'S_n',  'V_n',    'P',    'V',      'H',    'D',    'X_d',  'X_q',  'X_d_t',    'X_q_t',    'X_d_st',   'X_q_st',   'T_d0_t',   'T_q0_t',   'T_d0_st',  'T_q0_st'],
+                ['G1',      'B1',     100,     10,     80,  1.1314,      5,      0,     1.0,   0.66,    0.3,      0.66,       0.254,      0.273,       2.49,      10000,         0.06,       0.15],
+                ['IB',      'B3',    1e8,    132,    -80,  1,      1e5,      0,     1.05,   0.66,    0.328,      0.66,       0.254,      0.273,       2.49,      10000,         0.06,       0.15],
+            ],
+        },
+
+    }
+
+def load_model_wGovernor(R, D_t, V_min, V_max, T_1, T_2, T_3):
+    return {
+        'base_mva': 100, #'base_mva': system base/nominal complex power in [MVA].
+        'f': 50, #'f': system frequency in [Hz].
+        'slack_bus': 'B3', #'slack_bus': reference busbar with zero phase angle.
+
+        'buses': [
+            ['name',    'V_n'],
+            ['B1',         10],
+            ['B2',        132],
+            ['B3',        132],
+        ],
+        #'V_n': base/nominal voltage in [kV].
+
+        'lines': [
+            ['name',  'from_bus', 'to_bus',   'length',   'S_n',  'V_n',  'unit', 'R',    'X',   'B'],
+            ['L2-3_1',        'B2',     'B3',        250,      100,    132,    'PF',   0,    0.2,     0],
+            ['L2-3_2',        'B2',     'B3',        250,      110,    132,    'PF',   0,    0.2,     0],
+        ],
+
+
+        'transformers': [
+            ['name', 'from_bus', 'to_bus', 'S_n', 'V_n_from', 'V_n_to', 'R', 'X'],
+            ['T1',         'B1',     'B2',    100,         10,      132,   0, 0.1],
+        ],
+
+        'generators': {
+            'GEN': [
+                ['name',   'bus',  'S_n',  'V_n',    'P',    'V',      'H',    'D',    'X_d',  'X_q',  'X_d_t',    'X_q_t',    'X_d_st',   'X_q_st',   'T_d0_t',   'T_q0_t',   'T_d0_st',  'T_q0_st'],
+                ['G1',      'B1',     100,     10,     80,  1.1314,      5,      0,     1.0,   0.66,    0.3,      0.66,       0.254,      0.273,       2.49,      10000,         0.06,       0.15],
+                ['IB',      'B3',    1e8,    132,    -80,  1,      1e5,      0,     1.05,   0.66,    0.328,      0.66,       0.254,      0.273,       2.49,      10000,         0.06,       0.15],
+            ],
+        },
+
+        'gov': {
+            'TGOV1': [
+                ['name', 'gen', 'R', 'D_t', 'V_min', 'V_max', 'T_1', 'T_2', 'T_3'],
+                ['GOV1', 'G1', R, D_t, V_min, V_max, T_1, T_2, T_3],
+            ]
+        },
+
+    }
